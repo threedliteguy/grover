4df9a3cc0b668c1a2fc8cc848b1ea71c2d85dfc1
none <none>
2019-10-23 16:09:44 -0500

    add comment only re: 3D coin

--- src/main/scala/grover/GroverI.scala
+++ src/main/scala/grover/GroverI.scala
@@ -34,6 +34,27 @@ trait GroverI {
     ).map(_ * 0.5d)
   }
 
+  /*
+   Note: Above is standard 2D dispersion coin.  For a 3D Grover coin, could use:
+   
+   See: https://iopscience.iop.org/article/10.1088/1367-2630/15/7/073041
+   3D grover coin (6x6) sagemath code:
+
+import numpy
+ones=matrix( numpy.array(map(lambda x: int(x), numpy.ones((6,6)).ravel() )).reshape(6,6) )
+g3=(ones - 3*matrix.identity(6))*(1/3)
+
+[-2/3  1/3  1/3  1/3  1/3  1/3]
+[ 1/3 -2/3  1/3  1/3  1/3  1/3]
+[ 1/3  1/3 -2/3  1/3  1/3  1/3]
+[ 1/3  1/3  1/3 -2/3  1/3  1/3]
+[ 1/3  1/3  1/3  1/3 -2/3  1/3]
+[ 1/3  1/3  1/3  1/3  1/3 -2/3]
+
+  */
+
+
+
 
   val LEFT = 0
   val RIGHT = 1
